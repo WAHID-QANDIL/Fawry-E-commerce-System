875ceedfe3c5934536160dd115b12fee73054fd3
Wahid Ali <1213089@dtu.edu.eg>
2025-07-06 12:51:22 +0300

    Enhance main function: add error handling and improve checkout process output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,49 +6,52 @@ import modle.product.Shippable
 import service.checkout.CheckoutService
 
 fun main() {
+    try {
+        val products = dummyData
+        addDummyData()
+        addDummyDataShippable()
 
-    val products = dummyData
-    addDummyData()
-    addDummyDataShippable()
+        val customer = Customer()
+        customer.setBalance(1000.0) // Set initial balance for the customer
 
-    val customer = Customer()
-    customer.setBalance(100_000.0) // Set initial balance for the customer
-
-    customer.addToCart(
-        products.values.first(),
-        quantity = 6
-    )
-    //add shippable product
-    customer.addToCart(
-        products.values.first { it.canBeShipped != null },
-        quantity = 6
-    )
-    val checkoutService = CheckoutService()
-    val receipt = checkoutService.createCustomerReceipt(customer)
-    println("\n** Shipment notice **")
-    receipt.products.forEach {
-        val product = it.product
-        if (product.canBeShipped is Shippable) {
-            println("${it.quantity}x ${product.name} ${product.canBeShipped.weight.toInt()}Kg")
+        customer.addToCart(
+            products.values.first(),
+            quantity = 6
+        )
+        //add shippable product
+        customer.addToCart(
+            products.values.first { it.canBeShipped != null },
+            quantity = 6
+        )
+        val checkoutService = CheckoutService()
+        val receipt = checkoutService.createCustomerReceipt(customer)
+        println("\n** Shipment notice **")
+        receipt.products.forEach {
+            val product = it.product
+            if (product.canBeShipped is Shippable) {
+                println("${it.quantity}x ${product.name} ${product.canBeShipped.weight.toInt()}Kg")
+            }
         }
-    }
-    if (receipt.shippableItemSelectable.isEmpty()) {
-        println("No products to ship.")
-    } else {
-        println("Total package weight ${receipt.shippableItemSelectable.sumOf { (it.quantity * it.product.canBeShipped?.weight!!) }}kg")
-    }
+        if (receipt.shippableItemSelectable.isEmpty()) {
+            println("No products to ship.")
+        } else {
+            println("Total package weight ${receipt.shippableItemSelectable.sumOf { (it.quantity * it.product.canBeShipped?.weight!!) }}kg")
+        }
 
-    println("\n** Checkout receipt **")
-    receipt.products.forEach {
-        val product = it.product
-        println("${it.quantity}x ${product.name} ${product.price.toInt()}")
+        println("\n** Checkout receipt **")
+        receipt.products.forEach {
+            val product = it.product
+            println("${it.quantity}x ${product.name} ${product.price.toInt()}")
+        }
+        println("----------------------")
+        println("Subtotal ${receipt.subTotal.toInt()}")
+        println("Shipping ${receipt.shippingCost.toInt()}")
+        println("Amount ${receipt.totalAmount.toInt()}")
+        println("Checkout completed successfully.")
+        println("Balance after checkout: ${customer.getBalance().toInt()}")
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
     }
-    println("----------------------")
-    println("Subtotal ${receipt.subTotal.toInt()}")
-    println("Shipping ${receipt.shippingCost.toInt()}")
-    println("Amount ${receipt.totalAmount.toInt()}")
-    println("Balance after checkout: ${customer.getBalance().toInt()}")
-
 }
 //CONSOLE OUTPUT
 //** Shipment notice **
